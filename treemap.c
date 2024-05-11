4f2b548316a158e9c957b2657551246ce840b0fa
Your Name <you@example.com>
2024-05-11 13:15:12 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -125,6 +125,9 @@ void removeNode(TreeMap * tree, TreeNode* node)
             }
         }
         free(node->pair->key);
+        free(node->pair->value);
+        free(node->pair);
+        free(node);
     }
     else if (node->left == NULL || node->right == NULL)
     {
@@ -154,6 +157,9 @@ void removeNode(TreeMap * tree, TreeNode* node)
         {
             tree->root = hijo;  
         }
+        free(node->pair->key);
+        free(node->pair->value);
+        free(node->pair);
         free(node);
     }
     else if(node->left != NULL && node->right != NULL)
